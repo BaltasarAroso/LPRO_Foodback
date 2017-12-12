559bc10e71140ebc6e591d126f2670c616553543
danielgranhao <up201406280@fe.up.pt>
2017-12-12 15:50:28 +0000

    Added image download

--- Server/FoodBack/src/main/java/com/lpro/fbrest/resources/ImagesResource.java
+++ Server/FoodBack/src/main/java/com/lpro/fbrest/resources/ImagesResource.java
@@ -6,10 +6,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import javax.activation.MimetypesFileTypeMap;
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -42,7 +46,23 @@ public class ImagesResource {
 		saveFile(fileInputStream, "uploads/" + Integer.toString(client.getUsers_id()) 
 							+ fileName.substring(fileName.lastIndexOf('.')));
 		
+		//TODO - guardar na base de dados nome do ficheiro
+		
 		return Response.ok().build();
+	}
+	
+	@GET
+	@Path("/{image}")
+	@Produces("image/*")
+	public Response getImage(@PathParam("image") String image) {
+	  File f = new File("uploads/" + image);
+	 
+	  if (!f.exists()) {
+	    throw new WebApplicationException(404);
+	  }
+	 
+	  String mt = new MimetypesFileTypeMap().getContentType(f);
+	  return Response.ok(f, mt).build();
 	}
 
 	private void saveFile(InputStream fileInputStream, String filePath) {
