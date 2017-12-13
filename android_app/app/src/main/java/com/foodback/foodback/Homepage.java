b6dcdebdefdc9f7e91e9427f665ae22b9f2cdb69
AnabelaReigoto <up201405662@fe.up.pt>
2017-12-13 22:49:04 +0000

    Establishment Register

--- android_app/app/src/main/java/com/foodback/foodback/Homepage.java
+++ android_app/app/src/main/java/com/foodback/foodback/Homepage.java
@@ -5,9 +5,8 @@ import android.os.Bundle;
 
 public class Homepage extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    /*protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-    }
+    }*/
 }
