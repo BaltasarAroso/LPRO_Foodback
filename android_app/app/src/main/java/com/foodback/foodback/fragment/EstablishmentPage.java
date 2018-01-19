75fc660a4364d832044627fb4810d48aa2eb0750
COMP12 <COMP01@COMP12.local>
2018-01-19 13:21:42 +0000

    estab page .java

--- android_app/app/src/main/java/com/foodback/foodback/fragment/EstablishmentPage.java
+++ android_app/app/src/main/java/com/foodback/foodback/fragment/EstablishmentPage.java
@@ -8,17 +8,25 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.foodback.foodback.R;
+import com.foodback.foodback.logic.Establishment;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class EstablishmentPage extends Fragment {
 
+    Establishment estab;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_establishment_page, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_establishment_page, container, false);
+
+        
+
+
+        return view;
     }
 
     @Override
