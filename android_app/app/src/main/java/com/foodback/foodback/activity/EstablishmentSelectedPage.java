cb05c0ea9056bc351d57a81602264f4a7b3b8cc1
BaltasarAroso <up201404125@fe.up.pt>
2018-01-21 12:10:03 +0000

    connection of establishment selected page to server

--- android_app/app/src/main/java/com/foodback/foodback/activity/EstablishmentSelectedPage.java
+++ android_app/app/src/main/java/com/foodback/foodback/activity/EstablishmentSelectedPage.java
@@ -1,32 +1,19 @@
 package com.foodback.foodback.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.foodback.foodback.R;
 import com.foodback.foodback.logic.Establishment;
-import com.foodback.foodback.utils.CategoryUtils;
 
 public class EstablishmentSelectedPage extends AppCompatActivity {
 
-    protected TextView editname, editaddress, editcontact, editavgprice, editzone, editcity;
-    protected Spinner editcategory;
+    protected TextView editname, editcategory, editaddress, editcontact, editavgprice, editzone, editcity, editschedule1, editschedule2, editrating;
     protected CheckBox editdelivery;
 
-    protected String name, category, address, email, contact, zone, city;
-    protected Integer avg_price;
-    protected Boolean delivery;
-
     protected Establishment estab;
-
-    CategoryUtils catUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +22,7 @@ public class EstablishmentSelectedPage extends AppCompatActivity {
 
         // Initializing Views
         editname = findViewById(R.id.estab_page_name);
-        //editcategory = findViewById(R.id.estab_page_category);
+        editcategory = findViewById(R.id.estab_page_category);
         editaddress = findViewById(R.id.estab_page_address);
         editcontact = findViewById(R.id.estab_page_contact);
         editavgprice = findViewById(R.id.estab_page_avg_price);
@@ -43,20 +30,19 @@ public class EstablishmentSelectedPage extends AppCompatActivity {
         editcity = findViewById(R.id.estab_page_city);
         editdelivery = findViewById(R.id.estab_page_delivery);
 
-        // fetch categories for spinner and asynchronously fill it
-        catUtils = new CategoryUtils();
-        catUtils.populateSpinner(EstablishmentSelectedPage.this, editcategory, null);
-
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             editname.setText(bundle.getString(estab.getName()));
+            editcategory.setText(bundle.getString(estab.getCategory()));
             editaddress.setText(bundle.getString(estab.getAddress()));
             editcontact.setText(bundle.getString(estab.getContact()));
             editavgprice.setText(bundle.getString(String.valueOf(estab.getAvg_price())));
             editzone.setText(bundle.getString(estab.getZone()));
             editcity.setText(bundle.getString(estab.getCity()));
+            editschedule1.setText(bundle.getString(estab.getSchedule1()));
+            editschedule2.setText(bundle.getString(estab.getSchedule2()));
             editdelivery.setText(bundle.getString(estab.getName()));
-
+            editrating.setText(bundle.getString(String.valueOf(estab.getRating())));
         }
     }
 
